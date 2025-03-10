0f5e87c2725a3854b3dcf65ded607d7c3b97d934
marwa-abusaa <s12028718@stu.najah.edu>
2025-03-10 17:25:36 +0200

    implement booking,cancel and modify booking methods for passenger

--- Services/PassengerService.cs
+++ Services/PassengerService.cs
@@ -6,5 +6,39 @@ namespace Airport_Ticket_Booking.Services
 {
     public class PassengerService
     {
+        private readonly BookingService _bookingService;
+
+        public PassengerService(BookingService bookingService)
+        {
+            _bookingService = bookingService;
+        }
+        public void BookFlight(Passenger passenger, Flight flight)
+        {
+            var booking = new Booking
+            {
+                BookingId = GenerateBookingId(),
+                FlightId = flight.FlightNumber,
+                PassengerId = passenger.Id,
+                Class = flight.Class,
+                Price = flight.Price
+            };
+            _bookingService.BookFlight(booking);
+        }
+
+        public void CancelBooking(int bookingId)
+        {
+            _bookingService.CancelBooking(bookingId);
+        }
+
+        public void ModifyBooking(Booking updateBooking)
+        {
+            _bookingService.ModifyBooking(updateBooking);
+        }
+        
+        private int GenerateBookingId()
+        {
+            return new Random().Next(1, 99999);
+        }
     }
+}
 }
