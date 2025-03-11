0ff0c8009abb3b5f87761a2fe2e4328f29c065d5
marwa-abusaa <s12028718@stu.najah.edu>
2025-03-11 13:48:33 +0200

    implement flight validation

---
+++ Validation/FlightValidator.cs
@@ -0,0 +1,45 @@
+﻿using Airport_Ticket_Booking.Domain.FlightManagement;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airport_Ticket_Booking.Validation
+{
+    public class ValidationDetails
+    {
+        public string PropertyName { get; set; }
+        public string Type { get; set; }
+        public List<string> Constraints { get; set; } = new List<string>();
+    }
+
+    public class FlightValidator
+    {
+        public static List<ValidationDetails> GetValidationDetails()
+        {
+            var validationDetails = new List<ValidationDetails>();
+            var properties = typeof(Flight).GetProperties();
+
+            foreach (var property in properties)
+            {
+                var details = new ValidationDetails
+                {
+                    PropertyName = property.Name,
+                    Type = property.PropertyType.Name
+                };
+
+                var attributes = property.GetCustomAttributes(typeof(ValidationAttribute), true);
+                foreach (ValidationAttribute attribute in attributes)
+                {
+                    details.Constraints.Add(attribute.ErrorMessage);
+                }
+
+                validationDetails.Add(details);
+            }
+
+            return validationDetails;
+        }
+    }
+}
---
+++ Validation/FutureDateAttribute.cs
@@ -0,0 +1,17 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace Airport_Ticket_Booking.Validation
+{
+    public class FutureDateAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            if (value is DateTime date)
+            {
+                return date > DateTime.Now;
+            }
+            return false;
+        }
+    }
+}
