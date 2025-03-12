81db93f634b53069abce2c7d9ee5e066d5ecb241
marwa-abusaa <s12028718@stu.najah.edu>
2025-03-12 18:29:11 +0200

    refactor validation, implement ParesdFlight

--- Validation/FlightValidator.cs
+++ Validation/FlightValidator.cs
@@ -1,45 +1,119 @@
 ﻿using Airport_Ticket_Booking.Domain.FlightManagement;
-using System;
-using System.Collections.Generic;
+using Airport_Ticket_Booking.Domain.General;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
+
 
 namespace Airport_Ticket_Booking.Validation
 {
-    public class ValidationDetails
-    {
-        public string PropertyName { get; set; }
-        public string Type { get; set; }
-        public List<string> Constraints { get; set; } = new List<string>();
-    }
-
+  
     public class FlightValidator
     {
-        public static List<ValidationDetails> GetValidationDetails()
+        public Dictionary<string, string> GetValidationConstraints()
         {
-            var validationDetails = new List<ValidationDetails>();
+            var constraints = new Dictionary<string, string>();
+
             var properties = typeof(Flight).GetProperties();
 
             foreach (var property in properties)
             {
-                var details = new ValidationDetails
-                {
-                    PropertyName = property.Name,
-                    Type = property.PropertyType.Name
-                };
+                var attributes = property.GetCustomAttributes(true);
+                var constraintDescription = new List<string>();
 
-                var attributes = property.GetCustomAttributes(typeof(ValidationAttribute), true);
-                foreach (ValidationAttribute attribute in attributes)
+                foreach (var attribute in attributes)
                 {
-                    details.Constraints.Add(attribute.ErrorMessage);
+                    if (attribute is RequiredAttribute)
+                    {
+                        constraintDescription.Add("Required");
+                    }
+                    else if (attribute is RangeAttribute range)
+                    {
+                        constraintDescription.Add($"Allowed Range: {range.Minimum} → {range.Maximum}");
+                    }
+                    else if (attribute is FutureDateAttribute)
+                    {
+                        constraintDescription.Add("Must be a future date");
+                    }
                 }
 
-                validationDetails.Add(details);
+                if (constraintDescription.Any())
+                {
+                    constraints[property.Name] = string.Join(", ", constraintDescription);
+                }
             }
 
-            return validationDetails;
+            return constraints;
         }
+        public List<ValidationResult> ValidateFlight(Flight flight)
+        {
+            var results = new List<ValidationResult>();
+            var context = new ValidationContext(flight);
+            Validator.TryValidateObject(flight, context, results, validateAllProperties: true);
+            return results;
+        }
+
+        public Flight ParesdFlight(string line, List<ValidationResult> validationResults)
+        {
+            var parts = line.Split(',');
+
+            var flight = new Flight();
+            if (parts.Length != 8)
+            {
+                validationResults.Add(new ValidationResult($"Invalid number of columns in line: {line}"));
+            }
+
+            if (!int.TryParse(parts[0], out int flightNumber))
+            {
+                validationResults.Add(new ValidationResult("Flight Number must be a valid integer."));
+            }
+            else
+            {
+                flight.FlightNumber = flightNumber;
+            }
+
+            flight.DepartureCountry = parts[1];
+            flight.DestinationCountry = parts[2];
+
+            if (!DateTime.TryParseExact(parts[3], "yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime departureDate))
+            {
+                validationResults.Add(new ValidationResult("Departure Date must be in the correct format (yyyy-MM-dd HH:mm:ss)."));
+            }
+            else if (departureDate <= DateTime.Now) 
+            {
+                validationResults.Add(new ValidationResult("Departure Date must be in the future."));
+            }
+            else
+            {
+                flight.DepartureDate = departureDate;
+            }
+
+            flight.DepartureAirport = parts[4];
+            flight.ArrivalAirport = parts[5];
+
+            if (!Enum.TryParse(parts[6], out FlightClassType flightClass))
+            {
+                validationResults.Add(new ValidationResult("Class must be a valid FlightClassType (Economy, Business, First_Class)."));
+            }
+            else
+            {
+                flight.Class = flightClass;
+            }
+
+            if (!decimal.TryParse(parts[7], CultureInfo.InvariantCulture, out decimal price))
+            {
+                validationResults.Add(new ValidationResult("Price must be a valid decimal number."));
+            }
+            else if (price < 0)
+            {
+                validationResults.Add(new ValidationResult("Price must be a positive number."));
+            }
+            else
+            {
+                flight.Price = price;
+            }
+
+            return flight;
+        }
+       
     }
 }
