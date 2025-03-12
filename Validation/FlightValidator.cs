543099d37acc7d02def494de0487ce00dcfdb429
marwa-abusaa <s12028718@stu.najah.edu>
2025-03-12 18:31:51 +0200

    implement GetValidationConstraints method

--- Validation/FlightValidator.cs
+++ Validation/FlightValidator.cs
@@ -9,7 +9,41 @@ namespace Airport_Ticket_Booking.Validation
   
     public class FlightValidator
     {
-        
+        public Dictionary<string, string> GetValidationConstraints()
+        {
+            var constraints = new Dictionary<string, string>();
+
+            var properties = typeof(Flight).GetProperties();
+
+            foreach (var property in properties)
+            {
+                var attributes = property.GetCustomAttributes(true);
+                var constraintDescription = new List<string>();
+
+                foreach (var attribute in attributes)
+                {
+                    if (attribute is RequiredAttribute)
+                    {
+                        constraintDescription.Add("Required");
+                    }
+                    else if (attribute is RangeAttribute range)
+                    {
+                        constraintDescription.Add($"Allowed Range: {range.Minimum} → {range.Maximum}");
+                    }
+                    else if (attribute is FutureDateAttribute)
+                    {
+                        constraintDescription.Add("Must be a future date");
+                    }
+                }
+
+                if (constraintDescription.Any())
+                {
+                    constraints[property.Name] = string.Join(", ", constraintDescription);
+                }
+            }
+
+            return constraints;
+        }
         public List<ValidationResult> ValidateFlight(Flight flight)
         {
             var results = new List<ValidationResult>();
