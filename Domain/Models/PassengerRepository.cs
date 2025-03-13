d94b89b39a60a5178567b64164a58d904fc027bb
marwa-abusaa <s12028718@stu.najah.edu>
2025-03-13 19:30:59 +0200

    add repository for passengers

---
+++ Domain/Models/PassengerRepository.cs
@@ -0,0 +1,25 @@
+﻿using Airport_Ticket_Booking.Infrastructure.Storage;
+
+namespace Airport_Ticket_Booking.Domain.Models;
+
+public class PassengerRepository
+{
+    private FileHandler _fileHandler;
+    private static readonly string _filePath = "C:\\Users\\hp\\source\\repos\\Airport Ticket Booking\\Infrastructure\\Data\\passengers.csv";
+
+    public PassengerRepository(FileHandler fileHandler)
+    {
+        _fileHandler = fileHandler;
+    }
+
+    public List<Passenger> GetAllPassengers()
+    {
+        return _fileHandler.ReadFromFile<Passenger>(_filePath);
+    }
+
+    public void SavePassengerInfo(List<Passenger> allPassengers)
+    {
+        _fileHandler.WriteToFile<Passenger>(allPassengers, _filePath);
+
+    }
+}
